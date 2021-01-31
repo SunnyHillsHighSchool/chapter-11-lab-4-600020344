6c684fb82a67a6d5909e3b331f68fc6512315590
600020344 <77069279+600020344@users.noreply.github.com>
2021-01-31 04:32:33 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
   public static void main(String[] args) {
       //add test cases 
+      StringRemover a = new StringRemover("xR-MxR-MHelloxR-M","R-M");
+      System.out.println(a + "\n\n\n");
+      StringRemover b = new StringRemover("sxsssxssxsxssexssxsesss","xs");
+      System.out.println(b + "\n\n\n");
+      StringRemover c = new StringRemover("fuxqwexqwertyxqwexqwertyrtyxqwertyrtyn","qwerty");
+      System.out.println(c + "\n\n\n");
+      StringRemover d = new StringRemover("dogdogcatddodogdogdoggog","dog");
+      System.out.println(d);
   }
 }
